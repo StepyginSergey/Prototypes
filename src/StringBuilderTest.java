7e52ece3bd647c6145dc72695b50a87e6286f008
StepyginSA <sergey.stepygin@moex.com>
2019-04-19 16:35:20 +0300

    примеры 2

--- src/StringBuilderTest.java
+++ src/StringBuilderTest.java
@@ -1,7 +1,21 @@
 public class StringBuilderTest {
 
     public static void main(String[] args) {
+        //getLinkProperty();
+        System.out.println(getLongString(2000));
+    }
 
+    private static String getLongString(int n){
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            sb.append("a");
+        }
+
+        return sb.toString();
+    }
+
+    private static void getLinkProperty(){
         String links = "<links><link><property name=\"object_type\">1</property><property name=\"object_id\">4697</property></link></links>";
 
         links = links.replace("</links>","");
@@ -14,10 +28,9 @@ public class StringBuilderTest {
                 .append("<property name=\"object_type\">7</property>")
                 .append("<property name=\"object_id\">").append(ids).append("</property>")
                 .append("</link>")
-        .append("</links>");
+                .append("</links>");
 
         System.out.println(sBuilder);
-
 
         /*sBuilder.append("<links>");
 
@@ -27,8 +40,6 @@ public class StringBuilderTest {
                 .append("<property name=\"object_id\">").append(emitentInfo.getOrgId()).append("</property>")
                 .append("</link>");
         */
-
     }
-
 
 }
